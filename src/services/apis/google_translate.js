07430a54d217d3ebd621c94cce6dc5c474da4e4e
becs <angpohye@gmail.com>
2023-11-09 01:14:00 +0800

    added RajKKapadia's code on Google Translator API

---
+++ src/services/apis/google_translate.js
@@ -0,0 +1,49 @@
+const {Translate} = require('@google-cloud/translate').v2;
+require('dotenv').config({ path: '../../.env' }); //The ../../ part of the path indicates that you are going up two directories.
+
+// Your credentials
+const CREDENTIALS = JSON.parse(process.env.REACT_APP_CREDENTIALS);
+
+// Configuration for the client
+const translate = new Translate({
+    credentials: CREDENTIALS,
+    projectId: CREDENTIALS.project_id
+});
+
+const detectLanguage = async (text) => {
+
+    try {
+        let response = await translate.detect(text);
+        return response[0].language;
+    } catch (error) {
+        console.log(`Error at detectLanguage --> ${error}`);
+        return 0;
+    }
+}
+
+// detectLanguage('Oggi è lunedì')
+//     .then((res) => {
+//         console.log(res);
+//     })
+//     .catch((err) => {
+//         console.log(err);
+//     });
+
+const translateText = async (text, targetLanguage) => {
+
+    try {
+        let [response] = await translate.translate(text, targetLanguage);
+        return response;
+    } catch (error) {
+        console.log(`Error at translateText --> ${error}`);
+        return 0;
+    }
+};
+
+translateText('Oggi è lunedì', 'en')
+    .then((res) => {
+        console.log(res);
+    })
+    .catch((err) => {
+        console.log(err);
+    });
