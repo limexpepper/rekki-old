49546018d04b8bcf759133b063a435941efad0da
becs <angpohye@gmail.com>
2023-11-26 01:32:23 +0800

    (1)mounted <App /> on to index.html, (2) linked css files.

--- server.js
+++ server.js
@@ -1,13 +1,9 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
-// import { BrowserRouter } from 'react-router-dom'
-
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
 import express from 'express';
 
 const app = express(); 
-app.listen(3000, () => console.log('listening at 3000')); // recall, we want to create a web server so that we can use it to listen to requests
+app.listen(3001, () => console.log('listening at 3001')); // recall, we want to create a web server so that we can use it to listen to requests
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 app.use(express.static(join(__dirname, 'public')));
